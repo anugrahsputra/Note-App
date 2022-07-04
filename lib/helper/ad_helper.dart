f474becdd065c1456b32070cb5f7a6d5bfaf9bb4
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-07-05 05:17:15 +0700

    change android unit id to test unit id

--- lib/helper/ad_helper.dart
+++ lib/helper/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       return "ca-app-pub-3940256099942544/2934735716";
     } else {
